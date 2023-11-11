4681a3fcb490e61cfb4414e07f9c2b9882f73d98
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-11-11 23:31:06 +0300

    Add project files.

---
+++ TeaShopApi.DataAccessLayer/Context/TeaContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeaShopApi.EntityLayer.Concrete;
+
+namespace TeaShopApi.DataAccessLayer.Context
+{
+    public class TeaContext
+    {
+        public List<Drink> Drinks { get; set; }
+        public List<Testimonial> Testimonials { get; set; }
+        public List<Message> Messages { get; set; }
+        public List<Question>Questions  { get; set; }
+    }
+}
