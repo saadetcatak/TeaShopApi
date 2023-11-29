5dfd1e20aad844fda64d6772a6ae0626eed2a0f4
saadetcatakakgunes1211 <saadetcatak1453@gmail.com>
2023-11-29 13:07:33 +0300

    Api Crud

---
+++ TeaShopApi/Controllers/MessageController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeaShopApi.BusinessLayer.Abstract;
+using TeaShopApi.DtoLayer.MessageDtos;
+using TeaShopApi.EntityLayer.Concrete;
+
+namespace TeaShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+           var values= _messageService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDtos createMessageDtos)
+        {
+            Message message = new Message()
+            {
+                MessageDetail=createMessageDtos.MessageDetail,
+                MessageSenderName=createMessageDtos.MessageSenderName,
+                MessageSubject=createMessageDtos.MessageSubject,
+                MessageEmail=createMessageDtos.MessageEmail,
+                MessageSendDate=createMessageDtos.MessageSendDate,
+            };
+            _messageService.TInsert(message);
+            return Ok("Mesaj başarılı bir şekilde eklendi.");
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteMessage(int id) 
+        { 
+            var values=_messageService.TGetById(id);
+            _messageService.TDelete(values);
+            return Ok("Mesaj başarılı bir şekilde silindi.");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMessage(int id) 
+        {
+            var values = _messageService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDtos updateMessageDtos)
+        {
+            Message message = new Message()
+            {
+                MessageID = updateMessageDtos.MessageID,
+                MessageDetail = updateMessageDtos.MessageDetail,
+                MessageEmail = updateMessageDtos.MessageEmail,
+                MessageSendDate = updateMessageDtos.MessageSendDate,
+                MessageSenderName = updateMessageDtos.MessageSenderName,
+                MessageSubject = updateMessageDtos.MessageSubject,
+            };
+            _messageService.TUpdate(message);
+            return Ok("Mesaj başarılı bir şekilde sgüncellendi.");
+        }
+    }
+}
---
+++ TeaShopApi/Controllers/TestimonialController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeaShopApi.BusinessLayer.Abstract;
+using TeaShopApi.DtoLayer.TestimonialDtos;
+using TeaShopApi.EntityLayer.Concrete;
+
+namespace TeaShopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService _testimonialService;
+
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values=_testimonialService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost] 
+        public IActionResult CreateTestimonial(CreateTestimonialDto createTestimonialDto) 
+        {
+            Testimonial testimonial = new Testimonial()
+            {
+                TestimonialComment = createTestimonialDto.TestimonialComment,
+                TestimonialImageUrl = createTestimonialDto.TestimonialImageUrl,
+                TestimonialName = createTestimonialDto.TestimonialName
+            };
+            _testimonialService.TInsert(testimonial);
+            return Ok("Referans başarılı bir şekilde eklendi.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTestimonial(int id) 
+        {
+            var values=_testimonialService.TGetById(id);
+            _testimonialService.TDelete(values);
+            return Ok("Referans başarılı bir şekilde silindi.");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var values = _testimonialService.TGetById(id);
+            return Ok(values);
+        }
+
+        [HttpPut]
+         public IActionResult UpdateTestimonial(UpdateTestimonialDto updateTestimonialDto)
+        {
+            Testimonial testimonial = new Testimonial()
+            {
+                TestimonialComment = updateTestimonialDto.TestimonialComment,
+                TestimonialImageUrl = updateTestimonialDto.TestimonialImageUrl,
+                TestimonialName = updateTestimonialDto.TestimonialName,
+                TestimonialID = updateTestimonialDto.TestimonialID
+            };
+            _testimonialService.TUpdate(testimonial);
+            return Ok("Referans başarılı bir şekilde güncellendi.");
+        }
+    }
+}
